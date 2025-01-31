1c790c08d39e45667f096643bcc6b5d7c4e41fa9
Pankaj726655 <pp26421@gmail.com>
2025-01-31 22:39:58 +0530

    completed

---
+++ test.js
@@ -0,0 +1,14 @@
+const myAyy = []
+%debugPrint(myArr)
+
+// Types :  1. Continous
+//          2. Holey
+
+// SMI : (small integer)
+// Packed element
+// Double (float, string, function)
+
+const HoleyArr = [1,2,3, , 7, 5]
+
+
+// pass
